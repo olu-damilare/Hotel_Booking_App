9cdc85353d82307a5cdbd2ec6c782afc59b1ec07
olu-damilare <djolayemi@gmail.com>
2022-01-30 01:37:54 +0100

    required argmuments

--- lib/model/hotel.dart
+++ lib/model/hotel.dart
@@ -1,12 +1,24 @@
 
+import 'package:flutter/cupertino.dart';
+
 class Hotel {
 
-    int? id;
-    String? name;
-    String? iataCode;
-    String? subType;
-    String? hotelIds;
-    String? cityName;
-    String? countryCode;
+    int id;
+    String name;
+    String iataCode;
+    String subType;
+    String hotelIds;
+    String cityName;
+    String countryCode;
+
+    Hotel({
+        required this.id,
+        required this.name,
+        required this.iataCode,
+        required this.subType,
+        required this.hotelIds,
+        required this.cityName,
+        required this.countryCode});
+
 
 }
